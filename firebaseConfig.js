8d76563c20c2f7bf9b81b25831d0f2748b690561
Omama Khokhar <workwithomama@outlook.com>
2025-01-04 09:16:37 +0500

    Complete User Interface

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLQgWKCkzCMbXCfeyNiZNlHHhuC4uURgs",
+  authDomain: "e-commerce-website-3d74c.firebaseapp.com",
+  projectId: "e-commerce-website-3d74c",
+  storageBucket: "e-commerce-website-3d74c.firebasestorage.app",
+  messagingSenderId: "414660091373",
+  appId: "1:414660091373:web:471e2d1919f2e0641a5be4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
